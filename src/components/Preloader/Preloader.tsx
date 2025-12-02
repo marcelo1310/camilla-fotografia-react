b87d4143c74c056142f91aee773e46b054dbae7c
marcelo1310 <marcelotestes9010@gmail.com>
2025-12-01 22:40:36 -0300

    add preloader

---
+++ src/components/Preloader/Preloader.tsx
@@ -0,0 +1,8 @@
+export function Preloader() {
+  return (
+    <div className="fixed inset-0 flex flex-col gap-5 items-center justify-center bg-[var(--bege)] z-50 transition-opacity duration-500">
+      <div className="animate-spin rounded-full h-12 w-12 border-4 border-[var(--vinho)] border-t-[var(--branco)]"></div>
+      <p className="animate-pulse">Carregando...</p>
+    </div>
+  );
+}
--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -3,15 +3,32 @@ import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import { Contato } from "@/sections/Contato";
 import { Depoimentos } from "@/sections/Depoimentos";
+import { Preloader } from "@/components/Preloader";
+import { useEffect, useState } from "react";
 
 export function Layout() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 600);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
-      <Header />
-      <Outlet />
-      <Depoimentos />
-      <Contato />
-      <Footer />
+      {loading && <Preloader />}
+
+      <div
+        className={`${
+          loading ? "opacity-0" : "opacity-100"
+        } transition-opacity duration-700`}
+      >
+        <Header />
+        <Outlet />
+        <Depoimentos />
+        <Contato />
+        <Footer />
+      </div>
     </>
   );
 }
